95b30b2b046f441be27aa1898de3612d379f90f0
Kyleigh <kysmith13@gmail.com>
2019-04-25 10:37:13 -0700

    deploying to production

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const helmet = require('helmet')
+
 const MOVIEDEX = require('./moviedex.json')
 
 const app = express()
 
-console.log(process.env.API_TOKEN)
-
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
+app.use(helmet());
+
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validate bearer token')
 
     if (!authToken || authToken.split(' ')[1] != apiToken) {
         return res.status(400).json({error: 'Unauthorized request'})
@@ -55,7 +57,17 @@ app.get('/movie', function handleGetMovies(req, res) {
 
 })
 
-const PORT = 8000
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: {message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+const PORT = process.env.PORT || 7000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
